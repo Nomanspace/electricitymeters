7f5d2ff0c3fcb9f5efe7ffece17ea9a9ee8b2996
Nomanspace <Warhammerd@gmail.com>
2025-08-02 01:10:56 +0300

    finished work on bcd util

---
+++ app/src/main/java/org/nomanspace/electricitymeters/util/BinDataDecoder.java
@@ -0,0 +1,41 @@
+package org.nomanspace.electricitymeters.util;
+
+import java.util.Arrays;
+
+public class BinDataDecoder {
+
+    private static byte[] hexStringToByteArray(String string) {
+        byte[] binDataByte = new byte[string.length() / 2];
+        int z = 0;
+        for (int i = 0; i < string.length(); i = i + 2) {
+
+            int oneByteUnit = Integer.parseInt(string.substring(i, i + 2), 16);
+            binDataByte[z] = (byte) oneByteUnit;
+            z++;
+        }
+        return binDataByte;
+    }
+
+    private static int bcdByteToInt(Byte hexPair) {
+        int leftPairChunk = (hexPair >> 4) & 0x0F;
+        int rightPairChunk = hexPair & 0x0F;
+        return leftPairChunk * 10 + rightPairChunk;
+    }
+
+    private static int bcdBytesToInt(byte[] bcdBytes, int offset, int lengthForExecute) {
+        int result = 0;
+        for (int i = offset + lengthForExecute - 1; i >= offset; i--) {
+            int currentChunk = bcdByteToInt(bcdBytes[i]);
+            result = result * 100;
+            result = result + currentChunk;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String stringExample = "8523AF";
+
+        System.out.println(Arrays.toString(hexStringToByteArray(stringExample)));
+    }
+}
